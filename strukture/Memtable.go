c44646b52a6bf76adf698e6738acb2c2b2b483a8
Leon <leonjuric03@gmail.com>
2024-01-29 14:38:10 +0100

    Pocetak MemTablea
    Test 1

--- strukture/Memtable.go
+++ strukture/Memtable.go
@@ -1,38 +1,68 @@
 // -Potrebno je omoguciti da korisnik podesava osobine memtable-a. Za to nam fali config.json fajl.
 // -Potrebno je omoguciti i implementaciju sa skip listom. Za to nam fali SkipList.go fajl.
 
-
 package strukture
 
-include (
-	"fmt"
-)
+import "fmt"
 
-
-struct Memtable {
-	treshold int // Bazirano na ukupnoj memoriji zapisa, ne broju razlicitih zapisa.
-	size int
+type Memtable struct {
+	treshold    int // Bazirano na ukupnoj memoriji zapisa, ne broju razlicitih zapisa.
+	size        int
 	currentSize int
 
-	data[int]byte map
-	data *BTree
+	data1 map[string][]byte
+	// data2 *BTree
 	// data *SkipList - Dodati naknadno, kada neko uradi skip listu
 }
 
-struct NewMemtable() {
+func NewMemtable(threshold, size int) *Memtable {
 	// Kreira memtable. config.json govori koristi li se mapa ili b-stablo.
+	mt := Memtable{threshold, size, 0, nil}
+	mt.data1 = make(map[string][]byte)
+	mt.initializeMemtable()
+
+	return &mt
+
 }
 
-func add() {
+func (mt *Memtable) add(key int, value []byte) {
 	// Dodaje podatak u memtable.
+	//proveri da li imas prostora da se doda
+
+	mt.data1[key] = value
+	mt.currentSize = mt.currentSize + []byte.size()
+	// currentSize ukupnu velicina u memoriji
+	// ako nema mesta da se doda onda sta treba da se uradi
+	// ako je se dodaje u popunjen memtable //	1. Memtable se prazni i ponovo koristi;
 }
 
-func delete() {
-	// Brise podatak iz memtable-a.
+func (mt *Memtable) delete(key int) {
+	// Briše podatak iz memtable-a.
+	if mt.data1 != nil {
+		if _, exists := mt.data1[key]; exists {
+			delete(mt.data1, key)
+			mt.currentSize--
+		} else {
+			fmt.Printf("Kljuc %d ne postoji u memtable\n", key)
+		}
+	} else {
+		fmt.Println("Memtable je prazan, ne mozes brisati podatke")
+	}
 }
 
-func get() {
-	// Vraca podatak iz memtable-a.
+func (mt *Memtable) get(key int) (byte, bool) {
+	// Vraća podatak iz memtable-a.
+	if mt.data1 != nil {
+		if value, exists := mt.data1[key]; exists {
+			return value, true
+		} else {
+			fmt.Printf("Kljuc %d ne postoji u memtable\n", key)
+			return 0, false
+		}
+	} else {
+		fmt.Println("Memtable je prazan, ne mozes GET")
+		return 0, false
+	}
 }
 
 func initializeMemtable() {
@@ -54,6 +84,6 @@ func createSSTable() {
 	// Cekamo da neko prvo napravi SSTable.go.
 }
 
-func main() {
-	// Za testiranje.
-}
+// func main() {
+// 	// Za testiranje.
+// }
