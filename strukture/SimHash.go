9e63bf784b469c0a32991f4775c6437d94a82c0c
Leon <leonjuric03@gmail.com>
2024-01-31 23:53:34 +0100

    Popravljen SimHash

--- strukture/SimHash.go
+++ strukture/SimHash.go
@@ -2,7 +2,8 @@ package strukture
 
 import (
 	"bytes"
-	"encoding/gob"
+	"encoding/binary"
+	"fmt"
 	"regexp"
 	"strings"
 
@@ -35,10 +36,11 @@ func (sh *SimHash) calculateFingerprint() {
 	fingerprint := make([]int, hashLength)
 	words := strings.Split(sh.data, " ")
 
+	// fmt.Println(words)
 	for _, word := range words {
 		word = regexp.MustCompile("[^a-zA-Z0-9 ]+").ReplaceAllString(word, "")
+		// fmt.Print(word, hashLength)
 		hash := hashfunc.StringBinaryHash(word, hashLength)
-
 		for i := 0; i < hashLength; i++ {
 			if int(hash[i]) == 48 {
 				fingerprint[i]--
@@ -46,8 +48,8 @@ func (sh *SimHash) calculateFingerprint() {
 				fingerprint[i]++
 			}
 		}
+		// fmt.Println(word, hash)
 	}
-
 	for i := 0; i < hashLength; i++ {
 		if fingerprint[i] > 0 {
 			fingerprint[i] = 1
@@ -55,7 +57,7 @@ func (sh *SimHash) calculateFingerprint() {
 			fingerprint[i] = 0
 		}
 	}
-
+	fmt.Println("Finished calculating fingerprint:", fingerprint)
 	sh.fingerprint = fingerprint
 }
 
@@ -71,13 +73,36 @@ func (sh1 *SimHash) hammingDistance(sh2 *SimHash) int {
 	return hammingDistance
 }
 
-func (sh *SimHash) serializeSimHash() ([]byte, error) {
-	var buffer bytes.Buffer
-	encoder := gob.NewEncoder(&buffer)
-	err := encoder.Encode(sh)
+func serializeSimHash(sh *SimHash) ([]byte, error) {
+	data := []byte(sh.data)
+	hashLength := int32(sh.hashLength)
+	fingerprint := make([]int32, len(sh.fingerprint))
+	for i, v := range sh.fingerprint {
+		fingerprint[i] = int32(v)
+	}
 
-	if err != nil {
+	var buffer bytes.Buffer
+
+	if err := binary.Write(&buffer, binary.BigEndian, int32(len(data))); err != nil {
 		return nil, err
+	}
+
+	if err := binary.Write(&buffer, binary.BigEndian, data); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Write(&buffer, binary.BigEndian, hashLength); err != nil {
+		return nil, err
+	}
+
+	if err := binary.Write(&buffer, binary.BigEndian, int32(len(fingerprint))); err != nil {
+		return nil, err
+	}
+
+	for _, v := range fingerprint {
+		if err := binary.Write(&buffer, binary.BigEndian, v); err != nil {
+			return nil, err
+		}
 	}
 
 	return buffer.Bytes(), nil
@@ -85,37 +110,73 @@ func (sh *SimHash) serializeSimHash() ([]byte, error) {
 
 func deserializeSimHash(data []byte) (*SimHash, error) {
 	var sh SimHash
-	buffer := bytes.NewBuffer(data)
-	decoder := gob.NewDecoder(buffer)
 
-	err := decoder.Decode(&sh)
+	var dataLength int32
+	var hashLength int32
+	var fingerprintLength int32
 
-	if err != nil {
+	reader := bytes.NewReader(data)
+
+	if err := binary.Read(reader, binary.BigEndian, &dataLength); err != nil {
 		return nil, err
+	}
+
+	dataBytes := make([]byte, dataLength)
+	if err := binary.Read(reader, binary.BigEndian, &dataBytes); err != nil {
+		return nil, err
+	}
+	sh.data = string(dataBytes)
+
+	if err := binary.Read(reader, binary.BigEndian, &hashLength); err != nil {
+		return nil, err
+	}
+	sh.hashLength = int(hashLength)
+
+	if err := binary.Read(reader, binary.BigEndian, &fingerprintLength); err != nil {
+		return nil, err
+	}
+
+	sh.fingerprint = make([]int, fingerprintLength)
+	for i := 0; i < int(fingerprintLength); i++ {
+		var value int32
+		if err := binary.Read(reader, binary.BigEndian, &value); err != nil {
+			return nil, err
+		}
+		sh.fingerprint[i] = int(value)
 	}
 
 	return &sh, nil
 }
 
-/*
-func main() {
-	sh1 := NewSimHashWithFingerprint("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mollis lorem et tortor elementum, ac laoreet nisi finibus. Pellentesque consectetur felis turpis. Nulla consequat nibh ut mauris semper vehicula. Donec quis massa a dui rutrum dapibus. Sed a commodo eros. Phasellus venenatis ligula eget lorem tempus, eu molestie erat sagittis. Aenean auctor urna quis orci rhoncus, pellentesque ultrices ex ullamcorper. Curabitur arcu sem, laoreet vel sem ut, blandit vulputate mauris. Aenean posuere leo quam, vitae venenatis dolor bibendum placerat.", 8)
-	sh2 := NewSimHash("Nullam nunc odio, rutrum laoreet nisl non, venenatis lobortis risus. In lobortis lacus non malesuada varius. Aliquam pellentesque ligula at nibh gravida interdum. Suspendisse et ultricies dolor, ac rhoncus lectus. Donec varius ex eu turpis luctus pharetra. Aliquam blandit, nulla vel malesuada condimentum, tortor nisi sodales erat, ac semper leo lacus ut dui. Pellentesque consequat commodo massa et tempor. Suspendisse mattis, quam nec vulputate imperdiet, mauris felis iaculis risus, in tincidunt lorem elit eu purus. Nullam elementum neque mattis felis convallis, ut consequat leo aliquet.", 8)
-
-	sh1.calculateFingerprint()
-	sh2.calculateFingerprint()
-
-	fmt.Print("\nHamming distance sh1-sh2: ", sh1.hammingDistance(sh2))
-
-	var sh3 *SimHash
-	var data []byte
-
-	data, _ = sh1.serializeSimHash()
-	sh3, _ = deserializeSimHash(data)
-
-	sh3.calculateFingerprint() // Ovdje nastaje greska. sh3 se vraca kao nil.
-
-	fmt.Print("\nHamming distance sh1-sh3: ", sh3.hammingDistance(sh1))
-	fmt.Print("\nHamming distance sh2-sh3: ", sh3.hammingDistance(sh2))
+func (sh *SimHash) toString() string {
+	return sh.data
 }
-*/
+
+// func main() {
+// 	sh1 := NewSimHashWithFingerprint("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mollis lorem et tortor elementum, ac laoreet nisi finibus. Pellentesque consectetur felis turpis. Nulla consequat nibh ut mauris semper vehicula. Donec quis massa a dui rutrum dapibus. Sed a commodo eros. Phasellus venenatis ligula eget lorem tempus, eu molestie erat sagittis. Aenean auctor urna quis orci rhoncus, pellentesque ultrices ex ullamcorper. Curabitur arcu sem, laoreet vel sem ut, blandit vulputate mauris. Aenean posuere leo quam, vitae venenatis dolor bibendum placerat.", 8)
+// 	sh2 := NewSimHash("Nullam nunc odio, rutrum laoreet nisl non, venenatis lobortis risus. In lobortis lacus non malesuada varius. Aliquam pellentesque ligula at nibh gravida interdum. Suspendisse et ultricies dolor, ac rhoncus lectus. Donec varius ex eu turpis luctus pharetra. Aliquam blandit, nulla vel malesuada condimentum, tortor nisi sodales erat, ac semper leo lacus ut dui. Pellentesque consequat commodo massa et tempor. Suspendisse mattis, quam nec vulputate imperdiet, mauris felis iaculis risus, in tincidunt lorem elit eu purus. Nullam elementum neque mattis felis convallis, ut consequat leo aliquet.", 8)
+// 	sh1.calculateFingerprint()
+// 	sh2.calculateFingerprint()
+
+// 	fmt.Println("\nHamming distance sh1-sh2: ", sh1.hammingDistance(sh2))
+
+// 	serializedSh1, err := serializeSimHash(sh1)
+// 	if err != nil {
+// 		fmt.Println("Error during serialization:", err)
+// 		return
+// 	}
+// 	fmt.Println(serializedSh1, "\n", len(serializedSh1))
+
+// 	sh3, err := deserializeSimHash(serializedSh1)
+// 	if err != nil {
+// 		fmt.Println("Error during deserialization:", err)
+// 		return
+// 	}
+
+// 	fmt.Println("\nDeserialized sh3 fingerprint:", sh3.fingerprint)
+// 	fmt.Println("\nDeserialized sh3 data:", sh3.toString())
+
+// 	fmt.Print("\nHamming distance sh1-sh3: ", sh3.hammingDistance(sh1))
+// 	fmt.Print("\nHamming distance sh2-sh3: ", sh3.hammingDistance(sh2))
+
+// }
