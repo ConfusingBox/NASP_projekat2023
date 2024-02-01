d04f7f2c97f0b158f37bb588937f38577f013e0c
Leon <leonjuric03@gmail.com>
2024-02-01 01:08:33 +0100

    MEMTABLE SA GRESKAMA
    ima jos gresaka

--- strukture/Memtable.go
+++ strukture/Memtable.go
@@ -3,71 +3,111 @@
 
 package strukture
 
-import "fmt"
+import (
+	"errors"
+
+	"github.com/go-delve/delve/pkg/config"
+)
 
 type Memtable struct {
-	treshold    int // Bazirano na ukupnoj memoriji zapisa, ne broju razlicitih zapisa.
+	threshold   float32
 	size        int
 	currentSize int
 
-	data1 map[string][]byte
-	// data2 *BTree
-	// data *SkipList - Dodati naknadno, kada neko uradi skip listu
+	dataType     string
+	dataHashMap  map[string][]byte
+	dataSkipList *SkipList
+	dataBTree    *BTree
 }
 
-func NewMemtable(threshold, size int) *Memtable {
-	// Kreira memtable. config.json govori koristi li se mapa ili b-stablo.
-	mt := Memtable{threshold, size, 0, nil}
-	mt.data1 = make(map[string][]byte)
-	mt.initializeMemtable()
+// NewMemtable creates a new Memtable based on the specified data type.
+func NewMemtable() (*Memtable, error) {
+	config, err := config.LoadConfig("config.json")
+	if err != nil {
+		return nil, err
+	}
 
-	return &mt
+	mt := Memtable{config.MemTableThreshold, config.MemTableSize, 0, config.MemTableType, make(map[string][]byte), NewSkipList(config.SkipListDepth), NewBTree(config.BTreeDegree)}
+	return &mt, nil
+}
 
+func (mt *Memtable) InsertSkipList(key string, value []byte) error {
+	exist := mt.dataSkipList.Search([]byte(key))
+
+	if exist == nil {
+		mt.dataSkipList.Insert([]byte(key), value)
+		mt.currentSize += 1
+
+		return nil
+	}
+	return errors.New("Same key already here lol")
+}
+
+func (mt *Memtable) InsertBTree(key string, value []byte) error {
+	err := mt.dataBTree.Insert([]byte(key), value)
+
+	if err != nil {
+		return err
+	}
+
+	mt.currentSize += 1
+	return nil
+}
+
+func (mt *Memtable) InsertHashMap(key string, value []byte) error {
+	_, exist := mt.dataHashMap[key]
+
+	if exist {
+		return errors.New("Same key already here lol")
+	}
+
+	mt.dataHashMap[key] = value
+	mt.currentSize += 1
+	return nil
 }
 
 func (mt *Memtable) add(key int, value []byte) {
 	// Dodaje podatak u memtable.
 	//proveri da li imas prostora da se doda
 
-	mt.data1[key] = value
-	mt.currentSize = mt.currentSize + []byte.size()
 	// currentSize ukupnu velicina u memoriji
 	// ako nema mesta da se doda onda sta treba da se uradi
 	// ako je se dodaje u popunjen memtable //	1. Memtable se prazni i ponovo koristi;
 }
 
-func (mt *Memtable) delete(key int) {
-	// Briše podatak iz memtable-a.
-	if mt.data1 != nil {
-		if _, exists := mt.data1[key]; exists {
-			delete(mt.data1, key)
-			mt.currentSize--
-		} else {
-			fmt.Printf("Kljuc %d ne postoji u memtable\n", key)
-		}
-	} else {
-		fmt.Println("Memtable je prazan, ne mozes brisati podatke")
-	}
-}
+// func (mt *Memtable) delete(key int) {
+// 	// Briše podatak iz memtable-a.
+// 	if mt.data1 != nil {
+// 		if _, exists := mt.data1[key]; exists {
+// 			delete(mt.data1, key)
+// 			mt.currentSize--
+// 		} else {
+// 			fmt.Printf("Kljuc %d ne postoji u memtable\n", key)
+// 		}
+// 	} else {
+// 		fmt.Println("Memtable je prazan, ne mozes brisati podatke")
+// 	}
+// }
 
-func (mt *Memtable) get(key int) (byte, bool) {
-	// Vraća podatak iz memtable-a.
-	if mt.data1 != nil {
-		if value, exists := mt.data1[key]; exists {
-			return value, true
-		} else {
-			fmt.Printf("Kljuc %d ne postoji u memtable\n", key)
-			return 0, false
-		}
-	} else {
-		fmt.Println("Memtable je prazan, ne mozes GET")
-		return 0, false
-	}
-}
+// func (mt *Memtable) get(key int) (byte, bool) {
+// 	// Vraća podatak iz memtable-a.
+// 	if mt.data1 != nil {
+// 		if value, exists := mt.data1[key]; exists {
+// 			return value, true
+// 		} else {
+// 			fmt.Printf("Kljuc %d ne postoji u memtable\n", key)
+// 			return 0, false
+// 		}
+// 	} else {
+// 		fmt.Println("Memtable je prazan, ne mozes GET")
+// 		return 0, false
+// 	}
+// }
 
 func initializeMemtable() {
 	// Poziva se pri kreiranju memtable-a. Popunjava ga zapisima iz WAL-a.
 	// Iskreno, nemam pojma sta to znaci. :/
+
 }
 
 func flush() {
